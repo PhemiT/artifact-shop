ad9ee69437dc6bc3a0edf81cc3c269af67156754
Olufemi Olubukunmi <phemiwrites@gmail.com>
2024-03-29 11:32:08 +0100

    test Model

--- client/src/modules/Products.tsx
+++ client/src/modules/Products.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import '../assets/styles/products.scss';
+import {Suspense} from 'react';
+import {Canvas} from '@react-three/fiber';
+import {OrbitControls} from '@react-three/drei';
+import Scene from '../Model'
 
 const Products:React.FC = () => {
   return (
-    <div>Products</div>
+    <div className='products_main uniform-margin'>
+      <h1>Shop our Collection</h1>
+      <div className='tdcomp'>
+        <Canvas>
+          <ambientLight />
+          <OrbitControls />
+          <Suspense fallback={null}>
+            <Scene />
+          </Suspense>
+        </Canvas>
+      </div>
+    </div>
   )
 }
 
