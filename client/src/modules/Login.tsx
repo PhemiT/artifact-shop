4885762407ce0e424ec0d4410d0788e1f2643742
Olufemi Olubukunmi <phemiwrites@gmail.com>
2024-04-03 09:13:58 +0100

    ;-(

--- client/src/modules/Login.tsx
+++ client/src/modules/Login.tsx
@@ -35,7 +35,7 @@ const Login: React.FC= () => {
     })
   }
 
-  const handleGoogleLogin = async () => {
+  const handleGoogleLogin = () => {
     window.location.href = `${process.env.REACT_APP_API_URL}/auth/google`;
   };
 
